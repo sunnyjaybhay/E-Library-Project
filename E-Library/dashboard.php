cce6eb6590d4ad45ef0b3acfe8c4ac7c33c776db
ITACHI <120294016+sunnyjaybhay@users.noreply.github.com>
2024-06-06 10:36:58 +0530

    Add files via upload

---
+++ E-Library/dashboard.php
@@ -0,0 +1,75 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['login'])==0)
+  { 
+header('location:index.php');
+}
+else{?>
+<!DOCTYPE html>
+<html >
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>E-Library | User Dash Board</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME STYLE  -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
+</head>
+<body>
+      <!------MENU SECTION START-->
+<?php include('includes/header.php');?>
+<!-- MENU SECTION END-->
+    <div class="content-wrapper">
+         <div class="container">
+        <div class="row pad-botm">
+            <div class="col-md-12">
+                <h4 class="header-line">User DASHBOARD</h4>
+                
+                            </div>
+
+        </div>
+             
+             <div class="row">
+
+
+<a href="listed-books.php">
+<div class="col-md-4 col-sm-4 col-xs-6">
+ <div class="alert alert-success back-widget-set text-center">
+ <i class="fa fa-book fa-5x"></i>
+<?php 
+$sql ="SELECT id from tblbooks ";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$listdbooks=$query->rowCount();
+?>
+<h3><?php echo htmlentities($listdbooks);?></h3>
+Books Listed
+</div></div></a>
+      
+        </div>    
+    </div>
+    </div>
+     <!-- CONTENT-WRAPPER SECTION END-->
+<?php include('includes/footer.php');?>
+      <!-- FOOTER SECTION END-->
+    <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+    <!-- CORE JQUERY  -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="assets/js/bootstrap.js"></script>
+      <!-- CUSTOM SCRIPTS  -->
+    <script src="assets/js/custom.js"></script>
+</body>
+</html>
+<?php } ?>
